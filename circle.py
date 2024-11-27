509e1adcd1b1151ca739f77ae57368de60f30a5d
mylaff <43293264+mylaff@users.noreply.github.com>
2024-11-28 00:56:33 +0300

    L-03: Documentation added

--- circle.py
+++ circle.py
@@ -2,9 +2,22 @@ import math
 
 
 def area(r):
+    """
+    Возвращает площадь окружности по заданному численному радиусу.
+
+    :param r: радиус окружности (int | float)
+    :return: площадь окружности
+    :rtype: float
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
-    return 2 * math.pi * r
+    """
+    Возвращает периметр окружности по заданному численному радиусу.
 
+    :param r: радиус окружности (int | float)
+    :return: периметр окружности
+    :rtype: float
+    """
+    return 2 * math.pi * r
